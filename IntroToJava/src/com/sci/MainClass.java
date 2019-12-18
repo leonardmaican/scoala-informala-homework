31f9ca966bef7f862e8b8cb0270088d2c011d690
leonardmaican <leonard.maican@yahoo.ro>
2019-12-18 19:45:24 +0200

    this is the first class on github

--- IntroToJava/src/com/sci/MainClass.java
+++ IntroToJava/src/com/sci/MainClass.java
@@ -3,5 +3,6 @@ package com.sci;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("What up?");
+        System.out.println("Updated class");
     }
 }
